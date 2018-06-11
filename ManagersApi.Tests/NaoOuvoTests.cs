a277a8afcea55caaf22675894de1c9aaf7db9558
FelipeBaltazar <felipe.baltazar@promob.com>
2018-06-11 18:09:19 -0300

    Tests implementations

---
+++ ManagersApi.Tests/NaoOuvoTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PodcastManager;
+using PodcastManager.Enums;
+using PodcastManager.Models.NaoOuvo;
+using SharpTestsEx;
+
+namespace ManagersApi.Tests
+{
+    [TestClass]
+    public class NaoOuvoTests
+    {
+        [TestMethod]
+        public void ApiTest()
+        {
+            var podcastManager = new Manager();
+            var currentManager = podcastManager.GetManager(PodcastType.NaoOuvo);
+            var episodeCollection = currentManager.GetPodcastListAsync().Result;
+
+            var lastNaoOuvo = episodeCollection.First() as Episode;
+            var timeDifference = DateTime.Now.Date - DateTime.Parse(lastNaoOuvo.PubDate).Date;
+            var apiIsWorking = timeDifference < TimeSpan.FromDays(7);
+
+            apiIsWorking.Should().Be(true);
+        }
+    }
+}
---
+++ ManagersApi.Tests/NerdCastTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PodcastManager;
+using PodcastManager.Enums;
+using PodcastManager.Models.NerdCast;
+using SharpTestsEx;
+
+namespace ManagersApi.Tests
+{
+    [TestClass]
+    public class NerdCastTests
+    {
+        [TestMethod]
+        public void ApiTest()
+        {
+            var podcastManager = new Manager();
+            var currentManager = podcastManager.GetManager(PodcastType.NerdCast);
+            var episodeCollection = currentManager.GetPodcastListAsync().Result;
+            var firstItem = episodeCollection.First();
+
+            (firstItem is Episode).Should().Be(true);
+
+            var lastNerdcast = firstItem as Episode;
+            var timeDifference = DateTime.Now.Date - lastNerdcast.PubDate.Date;
+            var apiIsWorking = timeDifference < TimeSpan.FromDays(7);
+
+            apiIsWorking.Should().Be(true);
+        }
+
+        public void DownloadFail()
+        {
+            var podcastManager = new Manager();
+            var currentManager = podcastManager.GetManager(PodcastType.NerdCast);
+            var success = currentManager.DownloadPodcastAsync(null,null);
+
+            success.Should().Be(false);
+        }
+    }
+}
